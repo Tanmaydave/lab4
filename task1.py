580374910479dac638f895a685908114adee4bd6
TanmayDave <tn505451@dal.ca>
2020-05-11 15:59:10 -0300

    last commit--task1.py

--- task1.py
+++ task1.py
@@ -15,4 +15,8 @@ def vocabulary_counter(a_file, start_line=1):
     for word in new_text:
         word_count_dict[word] = word_count_dict.get(word, 0) + 1
 
-   
+    total_words = len(new_text)
+    num_different_words = len(word_count_dict.keys())
+    varied_vocab_percent = float(num_different_words) / total_words
+
+    print ("Total number of words: " + str(total_words) + "\n" + "Number of different words: " + str(num_different_words) + "\n" + "Vocab Variance Percent: " + str(varied_vocab_percent))
