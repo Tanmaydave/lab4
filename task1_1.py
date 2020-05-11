7c2d9f575760c8a2bd0314966c86148dc956fda6
TanmayDave <tn505451@dal.ca>
2020-05-11 16:15:58 -0300

    function declarati--task1_1.py

---
+++ task1_1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 16:11:38 2020
+
+@author: Tanmay Dave
+"""
+
+
+import string
+
+def stripped_text(a_file):
+    new_text = []
+    fin = open(a_file)
+
+   
