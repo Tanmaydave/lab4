225e92e06194c2f1dde5023c5da907413b896a7c
TanmayDave <tn505451@dal.ca>
2020-05-11 15:57:03 -0300

    Second commit--task1.py

--- task1.py
+++ task1.py
@@ -6,4 +6,9 @@ def vocabulary_counter(a_file, start_line=1):
     lines = fin.readlines()
     lines = lines[start_line - 1:]
 
+    for line in lines:
+        stripped_line = line.strip().lower().translate(string.maketrans("",""), string.punctuation).split()
+        new_text += stripped_line
+
+
    
